fb3e011294171567993aebe2d663c323153e7eb1
seeker8 <chuyo_gk87@yahoo.com.mx>
2015-01-19 16:54:19 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', [
+    '$scope',
+    function($scope){
+        $scope.test = 'Hello world';
+        $scope.posts = [
+            'post1',
+            'post2',
+            'post3',
+            'post4'
+        ]
+    }]);
